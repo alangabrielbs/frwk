e9b1d677d5b369ec0b360910d05dd58b5e69a161
Alan Gabriel <contatoalangabriel@gmail.com>
2021-09-26 01:36:26 -0300

    Remove Main Component

--- src/components/Hero/stories.tsx
+++ src/components/Hero/stories.tsx
@@ -1,27 +1,29 @@
 import { Story, Meta } from '@storybook/react'
+import { Container } from 'components/Container'
 
 import Hero, { HeroProps } from '.'
 
 export default {
   title: 'Components/Hero',
-  component: Hero
+  component: Hero,
+  args: {
+    title: 'Thoughts, Notes & Tutorials',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra hendrerit placerat. Nunc dictum nunc fermentum leo blandit porta.'
+  }
 } as Meta
 
-export const Default: Story<HeroProps> = (args) => <Hero {...args} />
+export const Default: Story<HeroProps> = (args) => (
+  <Container>
+    <Hero {...args} />
+  </Container>
+)
 
-Default.args = {
-  title: 'Thoughts, Notes & Tutorials',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra hendrerit placerat. Nunc dictum nunc fermentum leo blandit porta.'
-}
-
-export const Mobile: Story<HeroProps> = (args) => <Hero {...args} />
-
-Mobile.args = {
-  title: 'Thoughts, Notes & Tutorials',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pharetra hendrerit placerat. Nunc dictum nunc fermentum leo blandit porta.'
-}
+export const Mobile: Story<HeroProps> = (args) => (
+  <Container>
+    <Hero {...args} />
+  </Container>
+)
 
 Mobile.parameters = {
   layout: 'fullscreen',
